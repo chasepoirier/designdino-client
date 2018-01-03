826f9efa7d90b7bd3da905e3e85d340384f6c31e
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-02 23:27:59 -0800

    push client to heroku

---
+++ src/components/customers/customers.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+import './customers.css'
+
+class Customers extends Component {
+	
+	constructor() {
+		super();
+		this.state = {
+			customers: []
+		}
+	}
+
+	componentDidMount() {
+		fetch('/customers')
+			.then(res => res.json())
+			.then(customers => this.setState({customers}, () => console.log(customers)))
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Customers</h2>
+				{this.state.customers.map(customer => 
+					<li key={customer.id}> {customer.firstName}, {customer.lastName} </li>
+				)}
+			</div>
+		);
+	}
+}	
+
+export default Customers;
