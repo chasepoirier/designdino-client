bc6a9c7a7b135e788d6333c72d9f887de9fa7964
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-17 20:36:43 -0500

    added nginx config file

---
+++ src/components/AllUsers.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class AllUsers extends Component {
+	componentWillMount() {
+		
+	}
+
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+AllUsers.propTypes = {
+	
+}
+
+export default connect()AllUsers;
