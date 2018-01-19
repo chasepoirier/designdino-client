15728c6604a339a3ada655facb7899939a9b9749
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-19 17:15:05 -0500

    updated fossil reducer

--- src/reducers/FossilReducer.js
+++ src/reducers/FossilReducer.js
@@ -3,7 +3,7 @@ import { CREATE_FOSSIL, FOSSIL_FETCHED } from "../types";
 export default function fossil(state = {loaded: false}, action = {}) {
   switch (action.type) {
     case CREATE_FOSSIL:
-    	return {...state, success: action.success, loaded: true };
+    	return {...state, success: action.success, loaded: false };
 	case FOSSIL_FETCHED:
 		return {...state, ...action.fossil, loaded: true}
     default:
