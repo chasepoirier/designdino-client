098f54cda1a0f3fc7c248c71295b9ae894d5d63f
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-21 14:22:53 -0500

    clear profile on refresh

--- src/actions/Profile.js
+++ src/actions/Profile.js
@@ -1,13 +1,22 @@
 import api from "../api";
-import { PROFILE_FETCHED } from '../types'
+import { PROFILE_FETCHED, CLEAR_CURRENT_PROFILE } from '../types'
 
 export const userProfile = user => ({
 	type: PROFILE_FETCHED,
 	user
 })
 
+export const clearCurrentProfile = user => ({
+	type: CLEAR_CURRENT_PROFILE,
+	user
+})
+
 
 export const getUserProfile = username => dispatch => {
+	let user = {
+		avatar: null
+	}
+	dispatch(clearCurrentProfile(user));
 	return api.profile.fetchUserProfile(username).then(user => {
 		return dispatch(userProfile(user))
 	})
--- src/reducers/ProfileReducer.js
+++ src/reducers/ProfileReducer.js
@@ -1,9 +1,11 @@
-import { PROFILE_FETCHED } from "../types";
+import { PROFILE_FETCHED, CLEAR_CURRENT_PROFILE } from "../types";
 
 export default function profile(state = {loaded: false}, action = {}) {
   switch (action.type) {
     case PROFILE_FETCHED:
     	return {...state, ...action.user, loaded: true };
+    case CLEAR_CURRENT_PROFILE:
+    	return {...state, ...action.user, loaded: false}
     default:
     	return state;
   }
